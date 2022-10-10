764f6bf25daa4d9aa72d177f5b132c64538a0ff1
Mahmudul Khan <mkhan8@mtb.com>
2022-10-10 11:31:39 -0400

    added a spring project

---
+++ src/main/java/com/GMPU/muqtafi/MuqtafiApplication.java
@@ -0,0 +1,13 @@
+package com.GMPU.muqtafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuqtafiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuqtafiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GMPU/muqtafi/MuqtafiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GMPU.muqtafi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuqtafiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
