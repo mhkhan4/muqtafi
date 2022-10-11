b6e445e574b1bfe02d03886e8e28be9a2dd90b1a
Mahmudul Khan <gmpu@Mahmuduls-MacBook-Pro.local>
2022-10-11 13:18:37 -0400

    added react

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import axios from "axios";
+import React, {useState, useEffect}  from "react";
+
+const fetchRules = () =>{
+  
+}
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App;
