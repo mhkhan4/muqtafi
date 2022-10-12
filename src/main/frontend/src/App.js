da7ccbce1df7380aabc07d42ebcf520ded564f06
Mahmudul Khan <gmpu@Mahmuduls-MacBook-Pro.local>
2022-10-11 20:27:02 -0400

    added navbar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import axios from "axios";
 import React, {useState, useEffect}  from "react";
+import Navbar from "./components/Navbar";
 
-const fetchRules = () =>{
-  
+const FetchRules = () =>{
+  axios.get("http://localhost:8080/api/rules").then(res =>{
+    console.log(res);
+  });
+  return <h1>muqtafi</h1>
 }
 
 function App() {
-  return <div className="App"></div>
+  return(
+  <div className="App">
+    <Navbar />
+    <FetchRules />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/main/frontend/src/components/Navbar.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='dashboard'>
+    <div className="dashboard-nav">
+        <header>
+          <a href="#!" className="menu-toggle">
+            <i className="fas fa-bars"></i>
+          </a>
+          <a href="/#" className="brand-logo">
+            <i className="fas fa-anchor"></i>
+            <span>MUQTAFI</span></a>
+        </header>
+
+        <nav className="dashboard-nav-list">
+          <a href="/#" className="dashboard-nav-item"><i className="fas fa-home"></i>Home</a>
+          <a href="/#" className="dashboard-nav-item active"><i className="fas fa-tachometer-alt"></i>Ideas</a>
+          <a href="/#" className="dashboard-nav-item"><i className="fas fa-file-upload"></i> Tasks </a>
+    
+          <div className="nav-item-divider"></div>
+          <a href="/#" className="dashboard-nav-item"><i className="fas fa-cogs"></i> Rules </a>
+        </nav>
+    </div>
+    <div className='dashboard-app'>
+
+      <div>
+        <h1>Hello</h1>
+      </div>
+        {/* <div className='dashboard-content'>
+            <div className='container'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <h1>Welcome back Jim</h1>
+                    </div>
+                    <div className='card-body'>
+                        <p>Your account type is: Administrator</p>
+                    </div>
+                </div>
+            </div>
+        </div> */}
+    </div>
+</div>
+  );
+}
+
+export default Navbar;
+
+/*
+<div className='dashboard-nav-dropdown'>
+            <a href="#!" className="dashboard-nav-item dashboard-nav-dropdown-toggle"><i className="fas fa-photo-video"></i> Media </a>
+                <div className='dashboard-nav-dropdown-menu'><a href="/#"
+                                                            className="dashboard-nav-dropdown-item">All</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Recent</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Images</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Video</a></div>
+            </div>
+            <div className='dashboard-nav-dropdown'><a href="#!" className="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
+                    className="fas fa-users"></i> Users </a>
+                <div className='dashboard-nav-dropdown-menu'><a href="/#"
+                                                            className="dashboard-nav-dropdown-item">All</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Subscribed</a><a
+                        href="/#"
+                        className="dashboard-nav-dropdown-item">Non-subscribed</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Banned</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">New</a></div>
+            </div>
+            <div className='dashboard-nav-dropdown'><a href="#!" className="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
+                    className="fas fa-money-check-alt"></i> Payments </a>
+                <div className='dashboard-nav-dropdown-menu'><a href="/#"
+                                                            className="dashboard-nav-dropdown-item">All</a><a
+                        href="/#" className="dashboard-nav-dropdown-item">Recent</a><a
+                        href="/#" className="dashboard-nav-dropdown-item"> Projections</a>
+                </div>
+            </div>
+*/
